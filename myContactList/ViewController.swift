4c6c6ca1b143298d3cdee57ae239a5c209c1ae8c
lucy <yuki_lcy@yahoo.com>
2019-05-12 02:04:15 +0700

    Initial Commit

---
+++ myContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myContactList
+//
+//  Created by user on 12/05/19.
+//  Copyright © 2019 lucy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
